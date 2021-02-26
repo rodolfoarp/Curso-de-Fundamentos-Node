3ed360dcbe33d0801cd92e086e10a0fc528eff3f
Rodolfo Ramirez <ramirezrodolfo333@gmail.com>
2021-02-26 10:22:31 -0430

    usos de la consola

---
+++ modulos/consola.js
@@ -0,0 +1,27 @@
+/*function function1(){
+    console.group('funcion 1')
+    console.log('Esto es de la funcion1')
+    console.log('Esto tambien')
+    console.log('Esto tambien')
+    funcion2();
+    console.log('Hemos vuelto a la 1')
+    console.groupEnd('funcion 1')
+}
+
+function funcion2(){
+    console.group('funcion 2')
+    console.log('Ahora estamos en la funcion 2')
+    console.groupEnd('funcion 2')
+}
+
+console.log('empezamos')
+function1(); */
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces')
+console.count('veces')
+console.count('veces')
